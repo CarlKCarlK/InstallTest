56e986d5db3b406ecc00d874cbf077c8a8127870
Carl Kadie <carlk@msn.com>
2020-05-21 09:02:40 -0700

    Add asserts to pysnptest minitest

--- pysnptools.minitest.py
+++ pysnptools.minitest.py
@@ -7,6 +7,8 @@ from pysnptools.distreader import Bgen
 bgen = Bgen(bgen_file)          # Create a reader
 probs0 = bgen[:,0].read().val   # Read 1st SNP
 print(probs0.shape)             # Shape of the NumPy array
+assert probs0.shape==(500,1,3)
 
 probs_all = bgen.read().val     # Read all variants
-print(probs_all.shape)          # Shape of the NumPy arra
+print(probs_all.shape)          # Shape of the NumPy array
+assert probs_all.shape==(500,199,3)
