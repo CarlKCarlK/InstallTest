d3b5c63422071d678154cd6f9a4d96d4df100cca
Carl Kadie <carlk@msn.com>
2021-02-20 08:42:35 -0800

    test

--- fastlmm.minitest.py
+++ fastlmm.minitest.py
@@ -1,3 +1,6 @@
 import fastlmm
 import fastlmm.util.matrix.cample as cample
+from fastlmm.util.stats.quadform.qfc_src import wrap_qfc
+from fastlmm.association import single_snp_scale
+
 print("OK")
