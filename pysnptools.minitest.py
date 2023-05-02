072ca586b72358717dbc15d52ef2400f3b90030a
Carl Kadie <carlk@msn.com>
2023-05-02 14:19:11 -0700

    get bed-reader 34 from pypi

--- pysnptools.minitest.py
+++ pysnptools.minitest.py
@@ -1,14 +1,29 @@
 # Download a sample file
 from pysnptools.util import example_file
+import sys
+import platform
+
 bgen_file = example_file("pysnptools/examples/example.bgen")
 
-# Read from the file
-from pysnptools.distreader import Bgen
-bgen = Bgen(bgen_file)          # Create a reader
-probs0 = bgen[:,0].read().val   # Read 1st SNP
-print(probs0.shape)             # Shape of the NumPy array
-assert probs0.shape==(500,1,3)
+if not (platform.system() == 'Windows' and sys.version_info >= (3, 10)):
+    # Read from the file
+    from pysnptools.distreader import Bgen
+    bgen = Bgen(bgen_file)          # Create a reader
+    probs0 = bgen[:,0].read().val   # Read 1st SNP
+    print(probs0.shape)             # Shape of the NumPy array
+    assert probs0.shape==(500,1,3)
 
-probs_all = bgen.read().val     # Read all variants
-print(probs_all.shape)          # Shape of the NumPy array
-assert probs_all.shape==(500,199,3)
+    probs_all = bgen.read().val     # Read all variants
+    print(probs_all.shape)          # Shape of the NumPy array
+    assert probs_all.shape==(500,199,3)
+
+bed_file = example_file("pysnptools/examples/toydata.5chrom.bed")
+from pysnptools.snpreader import Bed
+bed = Bed(bed_file,count_A1=False)
+val0 = bed[:,0].read().val   # Read 1st SNP
+print(val0.shape)             # Shape of the NumPy array
+assert val0.shape==(500,1)
+
+val_all = bed.read().val     # Read all variants
+print(val_all.shape)          # Shape of the NumPy array
+assert val_all.shape==(500,10_000)
